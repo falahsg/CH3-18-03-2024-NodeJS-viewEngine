5462b7a9c25b55f21cb84f3172db3de7a8d4ef7c
Falahsg <falahsg18@gmail.com>
2024-03-18 23:56:33 +0700

    fix routes add-product

--- routes/admin.js
+++ routes/admin.js
@@ -3,14 +3,22 @@ const path = require("path");
 const express = require("express");
 
 const rootDir = require("../util/path");
-const adminData = require("./admin");
 
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
-  // res.sendFile(path.join(rootDir, 'views', 'shop.ejs'));
-  const products = adminData.products;
-  res.render("shop", { title: "FSW Best", products: products });
+const products = [];
+
+// /admin/add-product => GET
+router.get("/add-product", (req, res, next) => {
+  res.sendFile(path.join(rootDir, "views", "add-product.html"));
+});
+
+// /admin/add-product => POST
+router.post("/add-product", (req, res, next) => {
+  console.log(req.body);
+  products.push({ title: req.body.title });
+  res.redirect("/");
 });
 
 module.exports = router;
+module.exports.products = products;
